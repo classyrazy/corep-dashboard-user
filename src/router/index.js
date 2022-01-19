6def2c9d86042f4ecc4adac409f3db9286977b81
classyrazy <classyrazy014@gmail.com>
2022-01-19 05:54:42 -0800

    minor refactor

--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,16 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
-    path: '/signup',
-    name: 'Signup',
-    component: () => import('../views/Signup.vue')
-  },
-  {
     path: '/',
     name: 'Login',
     component: () => import('../views/Login.vue')
   },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: () => import('../views/Signup.vue')
+  },
+  
 ]
 
 const router = createRouter({
